6e3707828ec02b021e24ed7d4652ae3fc4a4d20e
1337it <91686534+1337it@users.noreply.github.com>
2024-02-27 08:27:30 +0400

    Update hooks.py

--- custom_app/hooks.py
+++ custom_app/hooks.py
@@ -16,6 +16,7 @@ app_include_css = "/assets/custom_app/css/homepage.css"
 # include js, css files in header of web template
 web_include_css = "/assets/custom_app/css/custom_app.css"
 web_include_css = "/assets/custom_app/css/homepage.css"
+web_include_js = "/assets/custom_app/js/homepage.js"
 
 # include custom scss in every website theme (without file extension ".scss")
 # website_theme_scss = "custom_app/public/scss/website"
