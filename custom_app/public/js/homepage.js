16c47dc5d1dbb20ccc6b6c06e6dae6c76df94f34
1337it <91686534+1337it@users.noreply.github.com>
2024-03-04 07:59:52 +0400

    Update homepage.js

--- custom_app/public/js/homepage.js
+++ custom_app/public/js/homepage.js
@@ -1,3 +1,8 @@
-$("meta[name='theme-color']").attr('content', 'rgb(23,23,23)');
-$("meta[name='theme-color']").attr('media', '(prefers-color-scheme: dark)');
-$("head").append("<meta name='theme-color' content='rgb(255,255,255)' media='(prefers-color-scheme: light)'>");
+$("meta[name='theme-color']").attr('content', '#ffffff');
+let themetag = $('html').attr('data-theme');
+if (themetag = 'dark'){
+$("meta[name='theme-color']").attr('content', '#171717');
+}
+else if (themetag = 'dark'){
+$("meta[name='theme-color']").attr('content', '#ffffff');
+}
