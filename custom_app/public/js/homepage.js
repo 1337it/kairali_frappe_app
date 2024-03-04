e3bf05ce7b96d0bbb328f6b048c1407801cef7f5
1337it <91686534+1337it@users.noreply.github.com>
2024-03-04 07:18:38 +0400

    Update homepage.js

--- custom_app/public/js/homepage.js
+++ custom_app/public/js/homepage.js
@@ -1,2 +1,3 @@
 $("meta[name='theme-color']").attr('content', 'rgb(23,23,23)');
 $("meta[name='theme-color']").attr('media', '(prefers-color-scheme: dark)');
+$("head").append("<meta name='theme-color' content='rgb(255,255,255)' media='(prefers-color-scheme: light)'>");
