07ae13af2feb4c6d46ea9dedd7e236aa84165931
1337it <91686534+1337it@users.noreply.github.com>
2024-03-19 17:37:41 +0400

    Update custom.js

--- custom_app/public/js/custom.js
+++ custom_app/public/js/custom.js
@@ -1,5 +1,6 @@
 frappe.ui.keys.add_shortcut({
 description: "Stock Ledger",
-shortcut: "ctrl+m", action: ()
+shortcut: "ctrl+m",
+  action: () => {
 frappe.set_route("Report", "Stock Ledger Entry")
-})
+}
