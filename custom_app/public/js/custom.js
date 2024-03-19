2826f9951df6892e791130e05b4195e366fafe58
1337it <91686534+1337it@users.noreply.github.com>
2024-03-19 18:54:29 +0400

    Update custom.js

--- custom_app/public/js/custom.js
+++ custom_app/public/js/custom.js
@@ -4,6 +4,43 @@ shortcut: "ctrl+m",
   action: () => {
     const current_doc = $('.data-row.editable-row').parent().attr("data-name");
             const item_row = locals["Sales Invoice Item"][current_doc];
-frappe.set_route("Report", "Stock Ledger Entry", {"item_code": item_row.item_code});)
+    new frappe.ui.form.MultiSelectDialog({
+    doctype: "Stock Ledger Entry",
+    target: this.cur_frm,
+    setters: {voucher_type:null,
+              posting_date:null,
+              voucher_no:null,
+              qty_after_transaction:null,
+              incoming_rate:null,
+              outgoing_rate:null
+
+              
+
+    },
+    add_filters_group: 1,
+          columns: ["item_code",
+            "warehouse",
+            "posting_date",
+            "voucher_no",
+            "qty_after_transaction",
+            "actual_qty",
+            "company",
+            "incoming_rate",
+            "modified",
+            "voucher_type",
+            "modified_by",
+            "outgoing_rate",
+            "stock_value",
+            "incoming_rate"],
+    get_query() {
+        return {
+            filters: {"item_code": item_row.item_code}
+        }
+    },
+    action(selections) {
+       return query_args;
+    }
+});
+
 }
 })
