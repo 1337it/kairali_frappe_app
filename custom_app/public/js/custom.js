1f80e942b67f79cab6d2451e235ae507748a9cbf
1337it <91686534+1337it@users.noreply.github.com>
2024-03-19 17:57:39 +0400

    Update custom.js

--- custom_app/public/js/custom.js
+++ custom_app/public/js/custom.js
@@ -2,5 +2,8 @@ frappe.ui.keys.add_shortcut({
 description: "Stock Ledger",
 shortcut: "ctrl+m",
   action: () => {
-frappe.set_route("Report", "Stock Ledger Entry")
+    const current_doc = $('.data-row.editable-row').parent().attr("data-name");
+            const item_row = locals["Sales Invoice Item"][current_doc];
+frappe.set_route("Report", "Stock Ledger Entry", {"item_code": item_row.item_code});)
 }
+})
