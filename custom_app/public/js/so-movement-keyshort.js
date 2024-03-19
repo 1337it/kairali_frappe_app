1c71e3b8f22277dd6116aaa4a628ba0bb5900e30
1337it <91686534+1337it@users.noreply.github.com>
2024-03-19 20:38:44 +0400

    Create so-movement-keyshort.js

---
+++ custom_app/public/js/so-movement-keyshort.js
@@ -0,0 +1,46 @@
+frappe.ui.keys.add_shortcut({
+description: "Stock Ledger",
+shortcut: "ctrl+m",
+  action: () => {
+    const current_doc = $('.data-row.editable-row').parent().attr("data-name");
+            const item_row = locals["Sales Order Item"][current_doc];
+    new frappe.ui.form.MultiSelectDialog({
+    doctype: "Stock Ledger Entry",
+    target: this.cur_frm,
+    setters: {voucher_type:null,
+              posting_date:null,
+              voucher_no:null,
+              qty_after_transaction:null,
+              incoming_rate:null,
+              outgoing_rate:null
+
+              
+
+    },
+    add_filters_group: 1,
+          columns: ["item_code",
+            "warehouse",
+            "posting_date",
+            "voucher_no",
+            "qty_after_transaction",
+            "actual_qty",
+            "company",
+            "incoming_rate",
+            "modified",
+            "voucher_type",
+            "modified_by",
+            "outgoing_rate",
+            "stock_value",
+            "incoming_rate"],
+    get_query() {
+        return {
+            filters: {"item_code": item_row.item_code}
+        }
+    },
+    action(selections) {
+       return query_args;
+    }
+});
+
+}
+})
