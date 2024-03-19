49f62eb9884a35bf503ef06562433db5f6dc6638
1337it <91686534+1337it@users.noreply.github.com>
2024-03-19 16:27:24 +0400

    Update hooks.py

--- custom_app/hooks.py
+++ custom_app/hooks.py
@@ -13,6 +13,7 @@ app_license = "MIT"
 # include js, css files in header of desk.html
 app_include_css = "/assets/custom_app/css/homepage.css"
 app_include_js = "/assets/custom_app/js/homepage.js"
+app_include_js = "/assets/custom_app/js/custom.js"
 
 # include js, css files in header of web template
 web_include_css = "/assets/custom_app/css/custom_app.css"
