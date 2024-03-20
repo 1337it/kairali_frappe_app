c8949e6a958d89500f127f00d6174c13b5f43967
1337it <91686534+1337it@users.noreply.github.com>
2024-03-20 17:36:00 +0400

    Update hooks.py

--- custom_app/hooks.py
+++ custom_app/hooks.py
@@ -15,6 +15,7 @@ app_include_css = "/assets/custom_app/css/homepage.css"
 app_include_js = "/assets/custom_app/js/homepage.js"
 app_include_js = "/assets/custom_app/js/sinv-movement-keyshort.js"
 app_include_js = "/assets/custom_app/js/sinv-pricelist.js"
+app_include_js = "/assets/custom_app/js/backbutton.js"
 
 # include js, css files in header of web template
 web_include_css = "/assets/custom_app/css/custom_app.css"
