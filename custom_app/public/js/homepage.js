5c84fe20a3c22adb69817e836b5296ca0e1d442b
1337it <91686534+1337it@users.noreply.github.com>
2024-03-20 22:52:13 +0400

    Update homepage.js

--- custom_app/public/js/homepage.js
+++ custom_app/public/js/homepage.js
@@ -1,6 +1,261 @@
 if ($('html').attr('data-theme') == 'dark') {
 $("meta[name='theme-color']").attr('content', '#171717');
-}
+};
 else if ($('html').attr('data-theme') == 'light'){
 $("meta[name='theme-color']").attr('content', '#ffffff');
+};
+
+frappe.ui.keys.add_shortcut({
+description: "Page Backward",
+shortcut: "alt+left",
+  action: () => {
+    window.history.go(-1);
 }
+});
+frappe.ui.keys.add_shortcut({
+description: "Page Forward",
+shortcut: "alt+right",
+  action: () => {
+    window.history.go(+1);
+}
+});
+
+const backbutton = document.createElement('button');
+
+      backbutton.id = 'backbutton';
+
+      // Attach the "click" event to your button
+      backbutton.addEventListener('click', () => {
+            history.back();
+      });
+      document.body.appendChild(backbutton);
+
+frappe.ui.keys.add_shortcut({
+description: "Price Breakdown",
+shortcut: "alt+p",
+  action: () => {
+    const current_doc = $('.data-row.editable-row').parent().attr("data-name");
+            const item_row5 = locals["Sales Invoice Item"][current_doc];
+    new frappe.ui.form.MultiSelectDialog({
+    doctype: "Item Price",
+    target: this.cur_frm,
+    setters: {price_list=null,
+                    price_list_rate=null,
+                    valid_from=null,
+                    valid_upto=null,
+                    custom_block_price=null,
+                    custom_wholesale_price=null,
+              modified=null,
+            modified_by=null
+
+              
+
+    },
+    add_filters_group: 1,
+          columns: ["item_code",
+                    "price_list",
+                    "price_list_rate",
+                    "valid_from",
+                    "valid_upto",
+                    "custom_block_price",
+                    "custom_wholesale_price",
+            "modified",
+            "modified_by"
+    get_query() {
+        return {
+            filters: {"item_code": item_row5.item_code}
+        }
+    },
+    action(selections) {
+       return query_args;
+    }
+});
+
+}
+});
+
+frappe.ui.keys.add_shortcut({
+description: "Stock Ledger",
+shortcut: "alt+m",
+  action: () => {
+    const current_doc = $('.data-row.editable-row').parent().attr("data-name");
+            const item_row = locals["Purchase Invoice Item"][current_doc];
+    new frappe.ui.form.MultiSelectDialog({
+    doctype: "Stock Ledger Entry",
+    target: this.cur_frm,
+    setters: {voucher_type:null,
+              posting_date:null,
+              voucher_no:null,
+              qty_after_transaction:null,
+              incoming_rate:null,
+              outgoing_rate:null
+
+              
+
+    },
+    add_filters_group: 1,
+          columns: ["item_code",
+            "warehouse",
+            "posting_date",
+            "voucher_no",
+            "qty_after_transaction",
+            "actual_qty",
+            "company",
+            "incoming_rate",
+            "modified",
+            "voucher_type",
+            "modified_by",
+            "outgoing_rate",
+            "stock_value",
+            "incoming_rate"],
+    get_query() {
+        return {
+            filters: {"item_code": item_row.item_code}
+        }
+    },
+    action(selections) {
+       return query_args;
+    }
+});
+
+}
+})
+frappe.ui.keys.add_shortcut({
+description: "Stock Ledger",
+shortcut: "alt+m",
+  action: () => {
+    const current_doc = $('.data-row.editable-row').parent().attr("data-name");
+            const item_row = locals["Purchase Order Item"][current_doc];
+    new frappe.ui.form.MultiSelectDialog({
+    doctype: "Stock Ledger Entry",
+    target: this.cur_frm,
+    setters: {voucher_type:null,
+              posting_date:null,
+              voucher_no:null,
+              qty_after_transaction:null,
+              incoming_rate:null,
+              outgoing_rate:null
+
+              
+
+    },
+    add_filters_group: 1,
+          columns: ["item_code",
+            "warehouse",
+            "posting_date",
+            "voucher_no",
+            "qty_after_transaction",
+            "actual_qty",
+            "company",
+            "incoming_rate",
+            "modified",
+            "voucher_type",
+            "modified_by",
+            "outgoing_rate",
+            "stock_value",
+            "incoming_rate"],
+    get_query() {
+        return {
+            filters: {"item_code": item_row.item_code}
+        }
+    },
+    action(selections) {
+       return query_args;
+    }
+});
+
+}
+});
+frappe.ui.keys.add_shortcut({
+description: "Stock Ledger",
+shortcut: "alt+m",
+  action: () => {
+    const current_doc = $('.data-row.editable-row').parent().attr("data-name");
+            const item_row = locals["Sales Invoice Item"][current_doc];
+    new frappe.ui.form.MultiSelectDialog({
+    doctype: "Stock Ledger Entry",
+    target: this.cur_frm,
+    setters: {voucher_type:null,
+              posting_date:null,
+              voucher_no:null,
+              qty_after_transaction:null,
+              incoming_rate:null,
+              outgoing_rate:null
+
+              
+
+    },
+    add_filters_group: 1,
+          columns: ["item_code",
+            "warehouse",
+            "posting_date",
+            "voucher_no",
+            "qty_after_transaction",
+            "actual_qty",
+            "company",
+            "incoming_rate",
+            "modified",
+            "voucher_type",
+            "modified_by",
+            "outgoing_rate",
+            "stock_value",
+            "incoming_rate"],
+    get_query() {
+        return {
+            filters: {"item_code": item_row.item_code}
+        }
+    },
+    action(selections) {
+       return query_args;
+    }
+});
+
+}
+});
+
+frappe.ui.keys.add_shortcut({
+description: "Stock Movement",
+shortcut: "alt+m",
+  action: () => {
+    const current_doc = $('.data-row.editable-row').parent().attr("data-name");
+            const item_row1 = locals["Sales Order Item"][current_doc];
+    new frappe.ui.form.MultiSelectDialog({
+    doctype: "Stock Ledger Entry",
+    target: this.cur_frm,
+    setters: {voucher_type:null,
+              posting_date:null,
+              voucher_no:null,
+              qty_after_transaction:null,
+              incoming_rate:null,
+              outgoing_rate:null
+
+              
+
+    },
+    add_filters_group: 1,
+          columns: ["item_code",
+            "warehouse",
+            "posting_date",
+            "voucher_no",
+            "qty_after_transaction",
+            "actual_qty",
+            "company",
+            "incoming_rate",
+            "modified",
+            "voucher_type",
+            "modified_by",
+            "outgoing_rate",
+            "stock_value",
+            "incoming_rate"],
+    get_query() {
+        return {
+            filters: {"item_code": item_row1.item_code}
+        }
+    },
+    action(selections) {
+       return query_args;
+    }
+});
+
+}
+});
