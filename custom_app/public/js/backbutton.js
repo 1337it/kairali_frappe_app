b32cf4d6b087ac3f802e2f922cfe45453eca471e
1337it <91686534+1337it@users.noreply.github.com>
2024-03-20 17:47:38 +0400

    Update backbutton.js

--- custom_app/public/js/backbutton.js
+++ custom_app/public/js/backbutton.js
@@ -1,8 +1,5 @@
 const backbutton = document.createElement('button')
 
-      // Set the button text to 'Can you click me?'
-      button.innerText = 'Can you click me?'
-
       backbutton.id = 'backbutton'
 
       // Attach the "click" event to your button
