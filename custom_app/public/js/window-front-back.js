d5477575848061356ac2b15c90fe26a65d33a2ba
1337it <91686534+1337it@users.noreply.github.com>
2024-03-20 22:38:37 +0400

    Update window-front-back.js

--- custom_app/public/js/window-front-back.js
+++ custom_app/public/js/window-front-back.js
@@ -4,11 +4,11 @@ shortcut: "alt+left",
   action: () => {
     window.history.go(-1);
 }
-})
+});
 frappe.ui.keys.add_shortcut({
 description: "Page Forward",
 shortcut: "alt+right",
   action: () => {
     window.history.go(+1);
 }
-})
+});
