390e61641e6485f56046675cfc8b42205fb905a1
1337it <91686534+1337it@users.noreply.github.com>
2024-03-20 22:52:33 +0400

    Update hooks.py

--- custom_app/hooks.py
+++ custom_app/hooks.py
@@ -13,10 +13,6 @@ app_license = "MIT"
 # include js, css files in header of desk.html
 app_include_css = "/assets/custom_app/css/homepage.css"
 app_include_js = "/assets/custom_app/js/homepage.js"
-app_include_js = "/assets/custom_app/js/sinv-movement-keyshort.js"
-app_include_js = "/assets/custom_app/js/sinv-pricelist.js"
-app_include_js = "/assets/custom_app/js/backbutton.js"
-app_include_js = "/assets/custom_app/js/window-front-back.js"
 
 # include js, css files in header of web template
 web_include_css = "/assets/custom_app/css/custom_app.css"
