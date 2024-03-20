32ef95ec44ccd95c5a98f4e9c1e38c4e97bc09e6
1337it <91686534+1337it@users.noreply.github.com>
2024-03-20 22:59:42 +0400

    Update homepage.js

--- custom_app/public/js/homepage.js
+++ custom_app/public/js/homepage.js
@@ -1,7 +1,7 @@
 if ($('html').attr('data-theme') == 'dark') {
 $("meta[name='theme-color']").attr('content', '#171717');
 };
-else if ($('html').attr('data-theme') == 'light'){
+if ($('html').attr('data-theme') == 'light'){
 $("meta[name='theme-color']").attr('content', '#ffffff');
 };
 
