464f99492ff069929a08d2a8bbed7f8da20594ae
1337it <91686534+1337it@users.noreply.github.com>
2024-03-24 01:30:53 +0400

    Update homepage.js

--- custom_app/public/js/homepage.js
+++ custom_app/public/js/homepage.js
@@ -41,92 +41,165 @@ const drivebutton = document.createElement('button');
       document.body.appendChild(drivebutton);
 
 
+
 frappe.ui.keys.add_shortcut({
-  
-description: "Price Breakdown",
-shortcut: "ctrl+n",
-  action: () => {
-    const current_doc = $('.data-row.editable-row').parent().attr("data-name");
-    const curdoc = (cur_frm.doctype + " Item");
-            const item_row5 = locals[curdoc][current_doc];
-    const pricelist = cur_frm.doc.selling_price_list;
-    new frappe.ui.form.MultiSelectDialog({
-    doctype: "Item Price",
-    target: this.cur_frm,
-    setters: {item_code:item_row5.item_code,
-      price_list_rate:null,
-                    custom_block_price:null,
-                    custom_wholesale_price:null
-    },
-    add_filters_group: 1,
-          columns: ["item_code",
-                    "price_list",
-                    "price_list_rate",
-                    "valid_from",
-                    "valid_upto",
-                    "custom_block_price",
-                    "custom_wholesale_price",
-            "modified",
-                    "custom_20",
-            "modified_by"],
-    get_query() {
-        return {
-            filters: {"item_code": item_row5.item_code, "price_list": pricelist}
-        }
-    },
-    action(selections) {
-       return query_args;
-    }
-});
-
-}
-});
-
-
-  frappe.ui.keys.add_shortcut({
 description: "Stock Ledger",
 shortcut: "ctrl+m",
   action: () => {
     const current_doc = $('.data-row.editable-row').parent().attr("data-name");
-    const curdoc = (cur_frm.doctype + " Item");
+     const curdoc = (cur_frm.doctype + " Item");
             const item_row = locals[curdoc][current_doc];
-    new frappe.ui.form.MultiSelectDialog({
-    doctype: "Stock Ledger Entry",
-    target: this.cur_frm,
-    setters: {voucher_type:null,
-              posting_date:null,
-              voucher_no:null,
-              qty_after_transaction:null,
-              incoming_rate:null,
-              outgoing_rate:null
+ const buying_price = frappe.db.get_list('Stock Ledger Entry', {
+        fields: ['posting_date', 'voucher_type', 'voucher_no', 'qty_after_transaction', 'incoming_rate', 'outgoing_rate'],
+        filters: [
+            ["Stock Ledger Entry", 'item_code', "=", item_row.item_code]
+        ],
+        as_list: 1
+    }).then(function(val) {
+        let data = frappe.utils.dict(['posting_date', 'voucher_type', 'voucher_no', 'qty_after_transaction', 'incoming_rate', 'outgoing_rate'], val);
+        const table_fields = [
+            {
+                label: 'Posted Date',
+                fieldname: 'posting_date',
+                fieldtype: 'Data',
+                in_list_view: 1,
+                read_only: 1
+            },
+           {
+                label: 'Voucher Type',
+                fieldname: 'voucher_type',
+                fieldtype: 'Data',
+                in_list_view: 1,
+                read_only: 1
+            },
+            {
+                label: 'Voucher No',
+                fieldname: 'voucher_no',
+                fieldtype: 'Link',
+                in_list_view: 1,
+                read_only: 1
+            },
+            {
+                label: 'Qty After Transaction',
+                fieldname: 'qty_after_transaction',
+                fieldtype: 'Data',
+                in_list_view: 1,
+                read_only: 1
+            },
+             {
+                label: 'Incoming Rate',
+                fieldname: 'incoming_rate',
+                fieldtype: 'Currency',
+                in_list_view: 1,
+                read_only: 1
+            },
+             {
+                label: 'Outgoing Rate',
+                fieldname: 'outgoing_rate',
+                fieldtype: 'Currency',
+                in_list_view: 1,
+                read_only: 1
+            }
+        ];
+        
+        let d = new frappe.ui.Dialog({
+            title: 'Item Movement: ' + item_row.item_code,
+            size: "large",
+            fields: [
+                {
+                    label: 'Movement',
+                    fieldname: 'items_movement',
+                    fieldtype: 'Table',
+                    fields: table_fields,
+                    options: 'Item',
+                    cannot_add_rows: 1,
+                    cannot_delete_rows : 1,
+                    data: data
+                },
+            ],
+            primary_action_label: 'Close',
+            primary_action(values) {
+                d.hide();
+             }
+        });
 
-              
-
-    },
-    add_filters_group: 1,
-          columns: ["item_code",
-            "warehouse",
-            "posting_date",
-            "voucher_no",
-            "qty_after_transaction",
-            "actual_qty",
-            "company",
-            "incoming_rate",
-            "modified",
-            "voucher_type",
-            "modified_by",
-            "outgoing_rate",
-            "stock_value",
-            "incoming_rate"],
-    get_query() {
-        return {
-            filters: {"item_code": item_row.item_code}
-        }
-    },
-    action(selections) {
-       return query_args;
-    }
+        d.show(); 
+   d.$wrapper.find('.modal-dialog').css("width", "90%");
+    });
+}
 });
 
+frappe.ui.keys.add_shortcut({
+description: "Stock Ledger",
+shortcut: "ctrl+n",
+  action: () => {
+    const current_doc = $('.data-row.editable-row').parent().attr("data-name");
+     const curdoc = (cur_frm.doctype + " Item");
+    const pricelist = cur_frm.doc.selling_price_list;
+            const item_row = locals[curdoc][current_doc];
+ const buying_price = frappe.db.get_list('Item Price', {
+        fields: ['item_code', 'custom_block_price', 'price_list_rate', 'custom_wholesale_price'],
+        filters: [
+            ["Item Price", 'item_code', "=", item_row.item_code],
+          ['price_list', "=", pricelist]
+        ],
+        as_list: 1
+    }).then(function(val) {
+        let data = frappe.utils.dict(["item_code", "custom_block_price", "price_list_rate", "custom_wholesale_price"], val);
+        const table_fields = [
+            {
+                label: 'Item Code',
+                fieldname: 'item_code',
+                fieldtype: 'Link',
+                options: 'Item',
+                in_list_view: 1,
+                read_only: 1
+            },
+           {
+                label: 'Block Price',
+                fieldname: 'custom_block_price',
+                fieldtype: 'Currency',
+                in_list_view: 1,
+                read_only: 1
+            },
+            {
+                label: 'Retail Price',
+                fieldname: 'price_list_rate',
+                fieldtype: 'Currency',
+                in_list_view: 1,
+                read_only: 1
+            },
+            {
+                label: 'Wholesale Price',
+                fieldname: 'custom_wholesale_price',
+                fieldtype: 'Currency',
+                in_list_view: 1,
+                read_only: 1
+            }
+        ];
+        
+        let d = new frappe.ui.Dialog({
+            title: 'Item Price: ' + item_row.item_code,
+            size: "large",
+            fields: [
+                {
+                    label: 'Items',
+                    fieldname: 'items_price',
+                    fieldtype: 'Table',
+                    fields: table_fields,
+                    options: 'Item',
+                    cannot_add_rows: 1,
+                    cannot_delete_rows : 1,
+                    data: data
+                },
+            ],
+            primary_action_label: 'Close',
+            primary_action(values) {
+                d.hide();
+             }
+        });
+
+        d.show();  
+    });
 }
 });
