c1f976d03013084db6e3516f832234b837f886b5
1337it <91686534+1337it@users.noreply.github.com>
2024-04-05 17:37:18 +0400

    Update queries.py

--- custom_app/controllers/queries.py
+++ custom_app/controllers/queries.py
@@ -141,7 +141,7 @@ def item_query(doctype, txt, searchfield, start, page_len, filters, as_dict=Fals
 
 	return frappe.db.sql(
 		"""select
-			it.item_name, CONCAT('Item Code:', RPAD(it.item_code , 15, ' ')) as label, CONCAT('Price:', RPAD(ip.price_list_rate , 8, ' ')) as price, CONCAT('Description:', RPAD(it.description , 50, ' ')) as description
+			it.item_name, CONCAT('Item Code:', RPAD(it.item_code , 15, SPACE(1))) as label, CONCAT('Price:', RPAD(ip.price_list_rate , 8, SPACE(1))) as price, CONCAT('Description:', RPAD(it.description , 50, SPACE(1))) as description
 		from tabItem it left join `tabItem Price` ip on ip.item_code = it.item_code AND ip.price_list = "Standard Selling"
 
 		where it.docstatus < 2
