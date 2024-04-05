5eed0348b7eb98b188a0d86b5049809f6387cd6a
1337it <91686534+1337it@users.noreply.github.com>
2024-04-05 15:39:51 +0400

    Update sales_invoice.js

--- custom_app/public/js/sales_invoice.js
+++ custom_app/public/js/sales_invoice.js
@@ -1,4 +1,4 @@
-frm.fields_dict['items'].grid.get_field("item_code").get_query = function() {
+cur_frm.fields_dict['items'].grid.get_field("item_code").get_query = function() {
     return {
         query: "custom_app.controllers.queries.item_query"
     }
