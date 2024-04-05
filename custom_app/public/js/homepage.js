29411df1072c1cae0d7d184a79e1977bac09f9c2
1337it <91686534+1337it@users.noreply.github.com>
2024-04-05 14:56:55 +0400

    Update homepage.js

--- custom_app/public/js/homepage.js
+++ custom_app/public/js/homepage.js
@@ -271,15 +271,5 @@ frappe.ui.keys.add_shortcut({
 });
 
 
-frappe.ui.form.on("Sales Invoice", "onload", function(frm,cdt,cdn) {	
-	frm.set_query("item_code", "items", function() {
-	    return {
-        		query: "custom_app.controllers.queries.item_query",
-		        filters: {"is_sales_item": 1}
-    		};
-	});								
-});
-
-
 
    
