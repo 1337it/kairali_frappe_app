5b3a0d7ac65e3ff193f96811272f3275238e5994
1337it <91686534+1337it@users.noreply.github.com>
2024-04-22 05:24:57 +0400

    Update search.py

--- custom_app/desk/search.py
+++ custom_app/desk/search.py
@@ -3,18 +3,27 @@ from frappe.utils import cstr, unique
 from frappe.desk.search import search_widget
 
 @frappe.whitelist()
-def search_link(doctype, txt, query=None, filters=None, page_length=20, searchfield=None, reference_doctype=None, ignore_user_permissions=False):
-    search_widget(doctype, txt.strip(), query, searchfield=searchfield, page_length=page_length, filters=filters, reference_doctype=reference_doctype, ignore_user_permissions=ignore_user_permissions)
-    frappe.response['results'] = build_for_autosuggest(frappe.response["values"])
-    del frappe.response["values"]
 
-def build_for_autosuggest(res):
-    results = []
-    for r in res:
-        out = {
-            "values": r[0],
-            "description": ", ".join(unique(cstr(d) for d in r if d)[1:]),
-            "actual_qty": get_actual_qty(r[0]) # get_actual_qty is self implement function
-        }
-        results.append(out)
-    return results
+def build_for_autosuggest(res: list[tuple], doctype: str) -> list[LinkSearchResults]:
+	def to_string(parts):
+		return ", ".join(
+			unique(_(cstr(part)) if meta.translated_doctype else cstr(part) for part in parts if part)
+		)
+
+	results = []
+	meta = frappe.get_meta(doctype)
+	if meta.show_title_field_in_link:
+		for item in res:
+			item = list(item)
+			if len(item) == 1:
+				item = [item[0], item[0]]
+			label = item[1]  # use title as label
+			item[1] = item[0]  # show name in description instead of title
+			if len(item) >= 3 and item[2] == label:
+				# remove redundant title ("label") value
+				del item[2]
+			results.append({"value": item[0], "label": label, "description": to_string(item[1:]), "price":price})
+	else:
+		results.extend({"value": item[0], "description": to_string(item[1:])} for item in res)
+
+	return results
