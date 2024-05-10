b622d2a307f0f2c22e2308690bd1312721e96eec
1337it <91686534+1337it@users.noreply.github.com>
2024-05-10 19:38:26 +0400

    Update homepage.js

--- custom_app/public/js/homepage.js
+++ custom_app/public/js/homepage.js
@@ -321,15 +321,27 @@ function maximize(){
 	$('body').removeClass('minimized');
 }
 
+var s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment-with-locales.min.js";
+$("head").append(s);
+var s1 = document.createElement("script");
+s1.type = "text/javascript";
+s1.src = "https://cdnjs.cloudflare.com/ajax/libs/gsap/1.16.1/TweenMax.min.js";
+$("head").append(s1);
+var s2 = document.createElement("script");
+s2.type = "text/javascript";
+s2.src = "https://code.jquery.com/jquery-2.2.4.min.js";
+$("head").append(s2);
 
 
-document.getElementById("chatbutton").append('<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment-with-locales.min.js"></script><script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.16.1/TweenMax.min.js"></script><script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>');
+
 
 const saver = document.createElement('div');
 saver.innerHTML='<div class="lockscreen"><svg><defs><radialGradient id="backHoleBelowClock" cx="50%" cy="50%" r="50%" fx="50%" fy="50%"><stop offset="50%"  style="stop-color:rgb(0,0,0);stop-opacity:0.7"/><stop offset="100%"	style="stop-color:rgb(0,0,0);stop-opacity:0"/></radialGradient><filter id="glow"><feGaussianBlur stdDeviation="2.5" result="coloredBlur"/><feMerge><feMergeNode in="coloredBlur"/><feMergeNode in="SourceGraphic"/></feMerge></filter><filter id="shadow" x="-20%" y="-20%" width="140%" height="140%"><feGaussianBlur in="SourceAlpha" stdDeviation="3" result="shadow"/><feOffset dx="1" dy="1"/><feMerge><feMergeNode/><feMergeNode in="SourceGraphic"/></feMerge></filter></defs><circle class="clockCircle hour" cx="500" cy="240" r="150" stroke-width="6" /><path id="arcHour" class="clockArc hour" stroke-width="6" stroke-linecap="round" filter="url(#glow)" /><circle class="clockDot hour" r="8" filter="url(#glow)" /><circle class="clockCircle minute" cx="500" cy="240" r="170" stroke-width="3" /><path id="arcMinute" class="clockArc minute" stroke-width="3" stroke-linecap="round" filter="url(#glow)" /><circle class="clockDot minute" r="5" filter="url(#glow)" /><text id="time" class="caption timeText" x="500" y="240" stroke-width="0" text-anchor="middle" alignment-baseline="middle"  filter="url(#shadow)"></text><text id="day" class="caption dayText" x="300" y="210" stroke-width="0" text-anchor="end" alignment-baseline="middle"  filter="url(#shadow)"></text><text id="date" class="caption dateText" x="300" y="250" stroke-width="0" text-anchor="end" alignment-baseline="middle" filter="url(#shadow)"></text></svg></div>';
 
       // Attach the "click" event to your button
-        $("body").attr('data-route', 'Saver');
+       
 body.appendChild(saver);
 
 
@@ -339,6 +351,7 @@ body.appendChild(saver);
 (function ($) {
       
     $( document ).on( "idle.idleTimer", function(event, elem, obj){
+
         $(".lockscreen").fadeIn();
     });
     
@@ -347,7 +360,7 @@ body.appendChild(saver);
     });
     
     $.idleTimer(3000);
-    
+  
 })(jQuery);
 
 
