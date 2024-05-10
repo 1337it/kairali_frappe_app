20f40154778720cb40d7e2f2980be67fa3faf5b1
1337it <91686534+1337it@users.noreply.github.com>
2024-05-10 19:06:30 +0400

    Update homepage.js

--- custom_app/public/js/homepage.js
+++ custom_app/public/js/homepage.js
@@ -320,3 +320,114 @@ function minimize(){
 function maximize(){
 	$('body').removeClass('minimized');
 }
+
+
+
+
+const saver = document.createElement('div');
+saver.innerHTML='<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment-with-locales.min.js"></script><script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.16.1/TweenMax.min.js"></script><script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>';
+
+      // Attach the "click" event to your button
+        $("body").attr('data-route', 'Saver');
+body.appendChild(saver);
+
+
+/*! Idle Timer v1.0.1 2014-03-21 | https://github.com/thorst/jquery-idletimer | (c) 2014 Paul Irish | Licensed MIT */
+!function(a){a.idleTimer=function(b,c){var d;"object"==typeof b?(d=b,b=null):"number"==typeof b&&(d={timeout:b},b=null),c=c||document,d=a.extend({idle:!1,timeout:3e4,events:"mousemove keydown wheel DOMMouseScroll mousewheel mousedown touchstart touchmove MSPointerDown MSPointerMove"},d);var e=a(c),f=e.data("idleTimerObj")||{},g=function(b){var d=a.data(c,"idleTimerObj")||{};d.idle=!d.idle,d.olddate=+new Date;var e=a.Event((d.idle?"idle":"active")+".idleTimer");a(c).trigger(e,[c,a.extend({},d),b])},h=function(b){var d=a.data(c,"idleTimerObj")||{};if(null==d.remaining){if("mousemove"===b.type){if(b.pageX===d.pageX&&b.pageY===d.pageY)return;if("undefined"==typeof b.pageX&&"undefined"==typeof b.pageY)return;var e=+new Date-d.olddate;if(200>e)return}clearTimeout(d.tId),d.idle&&g(b),d.lastActive=+new Date,d.pageX=b.pageX,d.pageY=b.pageY,d.tId=setTimeout(g,d.timeout)}},i=function(){var b=a.data(c,"idleTimerObj")||{};b.idle=b.idleBackup,b.olddate=+new Date,b.lastActive=b.olddate,b.remaining=null,clearTimeout(b.tId),b.idle||(b.tId=setTimeout(g,b.timeout))},j=function(){var b=a.data(c,"idleTimerObj")||{};null==b.remaining&&(b.remaining=b.timeout-(+new Date-b.olddate),clearTimeout(b.tId))},k=function(){var b=a.data(c,"idleTimerObj")||{};null!=b.remaining&&(b.idle||(b.tId=setTimeout(g,b.remaining)),b.remaining=null)},l=function(){var b=a.data(c,"idleTimerObj")||{};clearTimeout(b.tId),e.removeData("idleTimerObj"),e.off("._idleTimer")},m=function(){var b=a.data(c,"idleTimerObj")||{};if(b.idle)return 0;if(null!=b.remaining)return b.remaining;var d=b.timeout-(+new Date-b.lastActive);return 0>d&&(d=0),d};if(null===b&&"undefined"!=typeof f.idle)return i(),e;if(null===b);else{if(null!==b&&"undefined"==typeof f.idle)return!1;if("destroy"===b)return l(),e;if("pause"===b)return j(),e;if("resume"===b)return k(),e;if("reset"===b)return i(),e;if("getRemainingTime"===b)return m();if("getElapsedTime"===b)return+new Date-f.olddate;if("getLastActiveTime"===b)return f.lastActive;if("isIdle"===b)return f.idle}return e.on(a.trim((d.events+" ").split(" ").join("._idleTimer ")),function(a){h(a)}),f=a.extend({},{olddate:+new Date,lastActive:+new Date,idle:d.idle,idleBackup:d.idle,timeout:d.timeout,remaining:null,tId:null,pageX:null,pageY:null}),f.idle||(f.tId=setTimeout(g,f.timeout)),a.data(c,"idleTimerObj",f),e},a.fn.idleTimer=function(b){return this[0]?a.idleTimer(b,this[0]):this}}(jQuery);
+
+(function ($) {
+      
+    $( document ).on( "idle.idleTimer", function(event, elem, obj){
+        $(".lockscreen").fadeIn();
+    });
+    
+    $( document ).on( "active.idleTimer", function(event, elem, obj, triggerevent){
+        $(".lockscreen").fadeOut();
+    });
+    
+    $.idleTimer(3000);
+    
+})(jQuery);
+
+
+var describeArc, polarToCartesian, setCaptions;
+
+polarToCartesian = function(centerX, centerY, radius, angleInDegrees) {
+  var angleInRadians;
+  angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
+  return {
+    x: centerX + radius * Math.cos(angleInRadians),
+    y: centerY + radius * Math.sin(angleInRadians)
+  };
+};
+
+describeArc = function(x, y, radius, startAngle, endAngle) {
+  var arcSweep, end, start;
+  start = polarToCartesian(x, y, radius, endAngle);
+  end = polarToCartesian(x, y, radius, startAngle);
+  arcSweep = endAngle - startAngle <= 180 ? '0' : '1';
+  return ['M', start.x, start.y, 'A', radius, radius, 0, arcSweep, 0, end.x, end.y].join(' ');
+};
+
+setCaptions = function() {
+  var dot, hour, hourArc, minArc, minute, now, pos;
+  now = new Date();
+  hour = now.getHours() % 12;
+  minute = now.getMinutes();
+  hourArc = (hour * 60 + minute) / (12 * 60) * 360;
+  minArc = minute / 60 * 360;
+  $('.clockArc.hour').attr('d', describeArc(500, 240, 150, 0, hourArc));
+  $('.clockArc.minute').attr('d', describeArc(500, 240, 170, 0, minArc));
+  $('.clockDot.hour').attr('d', describeArc(500, 240, 150, hourArc - 3, hourArc));
+  $('.clockDot.minute').attr('d', describeArc(500, 240, 170, minArc - 1, minArc));
+  dot = $(".clockDot.hour");
+  pos = polarToCartesian(500, 240, 150, hourArc);
+  dot.attr("cx", pos.x);
+  dot.attr("cy", pos.y);
+  dot = $(".clockDot.minute");
+  pos = polarToCartesian(500, 240, 170, minArc);
+  dot.attr("cx", pos.x);
+  dot.attr("cy", pos.y);
+  return $('#time').text(moment().format('H:mm'));
+};
+
+$('#day').text(moment().format('dddd'));
+
+$('#date').text(moment().format('MMMM D'));
+
+setCaptions();
+
+setInterval(function() {
+  return setCaptions();
+}, 10 * 1000);
+
+$(function() {
+  TweenMax.staggerFrom(".clockArc", .5, {
+    drawSVG: 0,
+    ease: Power3.easeOut
+  }, 0.3);
+  TweenMax.from("#time", 1.0, {
+    attr: {
+      y: 350
+    },
+    opacity: 0,
+    ease: Power3.easeOut,
+    delay: 0.5
+  });
+  TweenMax.from(".dayText", 1.0, {
+    attr: {
+      y: 310
+    },
+    opacity: 0,
+    delay: 1.0,
+    ease: Power3.easeOut
+  });
+  return TweenMax.from(".dateText", 1.0, {
+    attr: {
+      y: 350
+    },
+    opacity: 0,
+    delay: 1.5,
+    ease: Power3.easeOut
+  });
+});
