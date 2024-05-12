7f6b389234a497679154627d359329b8bb73087d
1337it <91686534+1337it@users.noreply.github.com>
2024-05-12 18:09:26 +0400

    Update homepage.js

--- custom_app/public/js/homepage.js
+++ custom_app/public/js/homepage.js
@@ -1,3 +1,5 @@
+var clockheaad = '<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>'
+$('head').append(clockhead);
 if ($('html').attr('data-theme') == 'dark') {
 $("meta[name='theme-color']").attr('content', '#232323');
 };
