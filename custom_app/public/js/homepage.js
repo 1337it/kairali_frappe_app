5004c34c29d966516930ba5bc16382b0ba68b435
1337it <91686534+1337it@users.noreply.github.com>
2024-05-17 00:45:43 +0400

    Update homepage.js

--- custom_app/public/js/homepage.js
+++ custom_app/public/js/homepage.js
@@ -1,7 +1,23 @@
+
+
+$("div").each(function() { 
+  $(this).removeClass("formlist"); 
+}); 
+var onform = document.querySelectorAll("[style=''][data-page-route].editable-form");
+if (onform.length == 1) {
+	var dtype = this.doctype;
+var dname = this.doc
+document.getElementById('page-List\/'+dtype+'\/List').className += ' formlist';
+}
+
+console.log(container.length);
+
+var dtype = this.doctype;
+var dname = this.doc
+var slctr = '[data-route^="Form/'+cur_frm.doctype+'/'+cur_frm.docname+'"] #page-List\/'+cur_frm.doctype+'\/List { display:block!important;}' ;
+sheet.insertRule(slctr, sheet.cssRules.length);
+
 var dcontainer = document.querySelectorAll("[data-page-route^='List/'].page-container");
-
-
-
 console.log(dcontainer.length);
 if (dcontainer.length == 0 && this.docname != 0) {
     frappe.set_route("List", this.doctype);
