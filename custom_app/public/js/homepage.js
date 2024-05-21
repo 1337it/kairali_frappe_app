5f687124e675d7e03370877ec9ef0029eb2ea0ec
1337it <91686534+1337it@users.noreply.github.com>
2024-05-21 07:38:33 +0400

    Update homepage.js

--- custom_app/public/js/homepage.js
+++ custom_app/public/js/homepage.js
@@ -342,7 +342,7 @@ frappe.ui.keys.add_shortcut({
 
 		});
 
-
+if(frappe.get_route()[0] =='Form'){
 const routedoctype = cur_frm.doctype;
 const routedocname = cur_frm.docname;
 
@@ -350,6 +350,8 @@ frappe.set_route('List', routedoctype, 'List');
 
 	frappe.set_route('Form', routedoctype, routedocname);
 document.querySelectorAll('[data-page-route="List/' + routedoctype + '/List"]')[0].attributes.style.value ="";
+}
+
 
 
 
