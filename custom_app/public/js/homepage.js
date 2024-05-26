b67057cb45096e0aedbb7bf80ea6d109d90ef592
1337it <91686534+1337it@users.noreply.github.com>
2024-05-26 20:12:20 +0400

    Update homepage.js

--- custom_app/public/js/homepage.js
+++ custom_app/public/js/homepage.js
@@ -392,7 +392,11 @@ document.querySelectorAll('.navbar-collapse .navbar-nav')[0].prepend(drivebutton
 document.querySelectorAll('.navbar-collapse .navbar-nav')[0].prepend(chatbutton);
 
 document.querySelector("#body").appendChild(formclose);
-
+      var parent = document.querySelectorAll(`[data-name="${cur_frm.docname}"]`)[0].parentNode;
+var selecteditem = parent.parentNode;
+const bg = document.createAttribute("style");
+selecteditem.attributes.setNamedItem(bg);
+selecteditem.attributes.style.value ="background:#d9d9d9;mix-blend-mode:luminosity;";
 };
 
 
