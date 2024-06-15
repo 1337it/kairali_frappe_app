d7aee96538b2c7d6f0425b0fa2913f27e5dbc640
1337it <91686534+1337it@users.noreply.github.com>
2024-06-15 20:13:32 +0400

    Update homepage.js

--- custom_app/public/js/homepage.js
+++ custom_app/public/js/homepage.js
@@ -1,4 +1,33 @@
+const leftPane = document.querySelector("[data-page-route^='List/'].page-container");
+const rightPane = document.querySelector(".page-container:not(#page-List)");
+const gutter = document.querySelector(".gutter");
 
+
+function resizer(e) {
+  
+  window.addEventListener('mousemove', mousemove);
+  window.addEventListener('mouseup', mouseup);
+  
+  let prevX = e.x;
+  const leftPanel = leftPane.getBoundingClientRect();
+  
+  
+  function mousemove(e) {
+    let newX = prevX - e.x;
+    leftPane.style.width = leftPanel.width - newX + "px";
+  }
+  
+  function mouseup() {
+    window.removeEventListener('mousemove', mousemove);
+    window.removeEventListener('mouseup', mouseup);
+    
+  }
+  
+  
+}
+
+
+gutter.addEventListener('mousedown', resizer);
 
 
 
