fcaaafec8b154509a8841d5e408517418e2dcfbe
1337it <91686534+1337it@users.noreply.github.com>
2024-06-28 22:15:12 +0400

    Update homepage.js

--- custom_app/public/js/homepage.js
+++ custom_app/public/js/homepage.js
@@ -27,8 +27,8 @@ shortcut: "escape",
 })();
 
 
-
-
+var pwaworker = '<script> if (typeof navigator.serviceWorker !== "undefined") {navigator.serviceWorker.register("index.js")}</script>';
+$('body').append(pwaworker);
 var clockhead = '<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>'
 $('head').append(clockhead);
 if ($('html').attr('data-theme') == 'dark') {
