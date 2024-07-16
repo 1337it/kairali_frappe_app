4e21f9a2783cd46124660d8f67e19e3dd459b171
1337it <91686534+1337it@users.noreply.github.com>
2024-07-16 08:11:01 +0400

    Update homepage.js

--- custom_app/public/js/homepage.js
+++ custom_app/public/js/homepage.js
@@ -670,8 +670,8 @@ frappe.set_route("List",frappe.get_route()[1] ,"List");
 		document.getElementById("formclose").attributes.style.value ="display: none !important;";
 		document.getElementById('page-'+frappe.get_route()[1]).attributes.style.value ="width:97vw!important;";
   const formtitle = document.createAttribute("style");
-document.querySelector('[data-route^="Form/'+frappe.get_route()[1]+'"] .page-head .justify-between .page-title').attributes.setNamedItem(formtitle);
-document.querySelector('[data-route^="Form/'+frappe.get_route()[1]+'"] .page-head .justify-between .page-title').attributes.style.value ="padding-left:25px !important;";
+document.querySelector('[data-page-route^="Form/'+frappe.get_route()[1]+'"] .page-head .justify-between .page-title').attributes.setNamedItem(formtitle);
+document.querySelector('[data-page-route^="Form/'+frappe.get_route()[1]+'"] .page-head .justify-between .page-title').attributes.style.value ="padding-left:25px !important;";
 
       });	
 	subscribe.addEventListener("click", () => {
