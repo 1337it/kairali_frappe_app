4a335cb2019aa9d46252a465729e11f4c2a220be
1337it <91686534+1337it@users.noreply.github.com>
2024-07-23 12:40:16 +0400

    Update queries.py

--- custom_app/controllers/queries.py
+++ custom_app/controllers/queries.py
@@ -139,22 +139,94 @@ def item_query(doctype, txt, searchfield, start, page_len, filters, as_dict=Fals
 	doctype = "Item"
 	conditions = []
 
-	
+
+	if isinstance(filters, str):
+		filters = json.loads(filters)
+
+	# Get searchfields from meta and use in Item Link field query
+	meta = frappe.get_meta(doctype, cached=True)
+	searchfields = meta.get_search_fields()
+
+	columns = ""
+	extra_searchfields = [field for field in searchfields if field not in ["name", "description"]]
+
+	if extra_searchfields:
+		columns += ", " + ", ".join(extra_searchfields)
+
+	if "description" in searchfields:
+		columns += """, if(length(tabItem.description) > 40, \
+			concat(substr(tabItem.description, 1, 40), "..."), description) as description"""
+
+	searchfields = searchfields + [
+		field
+		for field in [
+			searchfield or "name",
+			"item_code",
+			"item_group",
+			"item_name",
+		]
+		if field not in searchfields
+	]
+	searchfields = " or ".join([field + " like %(txt)s" for field in searchfields])
+
+	if filters and isinstance(filters, dict):
+		if filters.get("customer") or filters.get("supplier"):
+			party = filters.get("customer") or filters.get("supplier")
+			item_rules_list = frappe.get_all(
+				"Party Specific Item",
+				filters={"party": party},
+				fields=["restrict_based_on", "based_on_value"],
+			)
+
+			filters_dict = {}
+			for rule in item_rules_list:
+				if rule["restrict_based_on"] == "Item":
+					rule["restrict_based_on"] = "name"
+				filters_dict[rule.restrict_based_on] = []
+
+			for rule in item_rules_list:
+				filters_dict[rule.restrict_based_on].append(rule.based_on_value)
+
+			for filter in filters_dict:
+				filters[scrub(filter)] = ["in", filters_dict[filter]]
+
+			if filters.get("customer"):
+				del filters["customer"]
+			else:
+				del filters["supplier"]
+		else:
+			filters.pop("customer", None)
+			filters.pop("supplier", None)
+
+	description_cond = ""
+	if frappe.db.count(doctype, cache=True) < 50000:
+		# scan description only if items are less than 50000
+		description_cond = "or tabItem.description LIKE %(txt)s"
+
 	return frappe.db.sql(
 		"""select
-			it.item_name, it.item_code as name, it.description as description, (SELECT sum(sl.actual_qty) FROM `tabStock Ledger Entry` sl WHERE sl.item_code = it.item_code GROUP BY sl.item_code) as available_qty
-from tabItem it
-		LEFT OUTER JOIN `tabStock Ledger Entry` iq ON it.item_name = iq.item_code
-  		where it.docstatus < 2
-			and it.disabled=0
-			and it.has_variants=0
-			and (it.end_of_life > %(today)s or ifnull(it.end_of_life, '0000-00-00')='0000-00-00')
-			and it.item_code IN (select parent from `tabItem Barcode` where barcode LIKE %(txt)s)
-    GROUP BY it.item_name,
-          it.item_code,
-          it.description,
-          iq.warehouse
-	limit %(start)s, %(page_len)s """.format(
+			it.tabItem.name {columns},  sum(iq.actual_qty)  as available_qty, ip.price_list_rate AS retail_rate
+		from tabItem it
+LEFT OUTER JOIN `tabStock Ledger Entry` iq ON it.item_name = iq.item_code
+LEFT OUTER JOIN `tabItem Price` ip  ON it.item_name = ip.item_code
+		where tabItem.docstatus < 2
+			and tabItem.disabled=0
+			and tabItem.has_variants=0
+			and (tabItem.end_of_life > %(today)s or ifnull(tabItem.end_of_life, '0000-00-00')='0000-00-00')
+			and ({scond} or tabItem.item_code IN (select parent from `tabItem Barcode` where barcode LIKE %(txt)s)
+				{description_cond})
+			{fcond} {mcond}
+		order by
+			if(locate(%(_txt)s, name), locate(%(_txt)s, name), 99999),
+			if(locate(%(_txt)s, item_name), locate(%(_txt)s, item_name), 99999),
+			idx desc,
+			name, item_name
+		limit %(start)s, %(page_len)s """.format(
+			columns=columns,
+			scond=searchfields,
+			fcond=get_filters_cond(doctype, filters, conditions).replace("%", "%%"),
+			mcond=get_match_cond(doctype).replace("%", "%%"),
+			description_cond=description_cond,
 		),
 		{
 			"today": nowdate(),
@@ -166,6 +238,8 @@ from tabItem it
 		as_dict=as_dict,
 	)
 
+	
+	
 
 @frappe.whitelist()
 @frappe.validate_and_sanitize_search_inputs
