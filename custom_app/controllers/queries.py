506c3b91be2cef60be5681071df43ef0cc23716f
1337it <91686534+1337it@users.noreply.github.com>
2024-07-26 15:55:40 +0400

    Update queries.py

--- custom_app/controllers/queries.py
+++ custom_app/controllers/queries.py
@@ -141,7 +141,7 @@ def item_query(doctype, txt, searchfield, start, page_len, filters, as_dict=Fals
 
 	return frappe.db.sql(
                 """select
-                        tabItem.name as name, tabItem.description as description, ip.price_list_rate AS retail_price, sum(iw.actual_qty) AS available_qty, iw.actual_qty AS in-store_qty, ia.alternative_item_code as substitute
+                        tabItem.name as name, tabItem.description as description, ip.price_list_rate AS retail_price, sum(iw.actual_qty) AS available_qty, iw.actual_qty AS in_store_qty, ia.alternative_item_code as substitute
 			from tabItem
    LEFT OUTER JOIN `tabItem Price` AS ip ON tabItem.item_code = ip.item_code
    LEFT OUTER JOIN `tabStock Ledger Entry` AS iw ON tabItem.item_code = iw.item_code
