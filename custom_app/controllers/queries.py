7cdddf8bf9216a4816127bcb22757a6cdeaf516b
1337it <91686534+1337it@users.noreply.github.com>
2024-07-26 20:42:50 +0400

    Update queries.py

--- custom_app/controllers/queries.py
+++ custom_app/controllers/queries.py
@@ -151,7 +151,7 @@ def item_query(doctype, txt, searchfield, start, page_len, filters, as_dict=Fals
                         and tabItem.disabled=0
                         and tabItem.has_variants=0
 			and (tabItem.name LIKE %(txt)s or ia.alternative_item_code LIKE %(txt)s or tabItem.description LIKE %(txt)s)
-   			and iw1.warehouse={fcond}
+   			and iw1.warehouse='Stores - AJ1'
 
 		group by tabItem.name, ia.item_code, tabItem.description, ia.alternative_item_code
   		order by tabItem.name, ia.item_code
