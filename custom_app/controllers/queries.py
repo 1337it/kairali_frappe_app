1644e944e3329d7a360f6366bc22b6cabec32061
1337it <91686534+1337it@users.noreply.github.com>
2024-07-26 16:09:24 +0400

    Update queries.py

--- custom_app/controllers/queries.py
+++ custom_app/controllers/queries.py
@@ -150,12 +150,12 @@ def item_query(doctype, txt, searchfield, start, page_len, filters, as_dict=Fals
                         and tabItem.disabled=0
                         and tabItem.has_variants=0
 			and (tabItem.name LIKE %(txt)s or ia.alternative_item_code LIKE %(txt)s or tabItem.description LIKE %(txt)s)
-   			and in-store_qty in {fcond}
+   			and in_store_qty in {fcond}
 
 		group by tabItem.name, ia.item_code, tabItem.description, ia.alternative_item_code,
   		order by tabItem.name, ia.item_code
 		limit %(start)s, %(page_len)s """.format(
-			fcond=get_filters_cond(doctype, filters, []),
+			fcond=get_filters_cond(`tabStock Ledger Entry`, filters, []),
 
 		),
 		{
