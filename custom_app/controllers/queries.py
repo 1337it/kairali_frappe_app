920652998d2326b4856d9310a18025a754b95a02
1337it <91686534+1337it@users.noreply.github.com>
2024-07-27 06:09:24 +0400

    Update queries.py

--- custom_app/controllers/queries.py
+++ custom_app/controllers/queries.py
@@ -152,6 +152,7 @@ def item_query(doctype, txt, searchfield, start, page_len, filters, as_dict=Fals
                         and tabItem.has_variants=0
 			and tabItem.name LIKE %(txt)s or ia.item_code LIKE %(txt)s or tabItem.description LIKE %(txt)s
    			{fcond}
+      		group by tabItem.name
   		order by tabItem.idx desc,
 			tabItem.item_code, tabItem.name
 		limit %(start)s, %(page_len)s """.format(
