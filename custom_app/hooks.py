c079424ec0c812e8eb8c8894477405afd969529e
Leet IT Solutions <91686534+1337it@users.noreply.github.com>
2025-08-15 18:15:08 +0400

    Update hooks.py

--- custom_app/hooks.py
+++ custom_app/hooks.py
@@ -11,6 +11,17 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
+app_include_js = [
+    "assets/custom_app/js/utils.js",
+    "assets/custom_app/js/boot.js",
+]
+
+# If you also want this on Website/Portal:
+website_include_js = [
+    "assets/custom_app/js/utils.js",
+    "assets/custom_app/js/boot.js",
+]
+website_include_css = ["assets/custom_app/css/custom_app.css"]
 app_include_css = "/assets/custom_app/css/homepage.css"
 app_include_js = "/assets/custom_app/js/homepage.js"
 app_include_js = "/assets/custom_app/js/login_whatsapp.js"
